377a747472f3eef121211fa0b62fce4b63a9bc31
Yuta Hoshino <y.hoshino25@gmail.com>
2022-04-05 21:55:02 +0900

    fetch data for blog-detail re-using custom_hook(useFetch)

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,12 +1,24 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetail = () => {
-  const { id } = useParams()
+  const { id } = useParams();
+  const url = 'http://localhost:8000/blogs/' + id
+  const { data:blog, isPending, error} = useFetch(url);  /* カスタムフック */
+  
   return ( 
     <div className="blog-detail">
-      <h2>Blog Detail - {id}</h2>
+      { error && <div>{ error }</div> }
+      { isPending && <div className="">Loading...</div> }      {/* when { here:Ture &&(then) here:is show  */}
+      { blog && (
+        <article>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+          <div>{ blog.body }</div>
+        </article>
+      )}
     </div>
-   );
+  );
 }
- 
+
 export default BlogDetail;
